7f2a902c7e1278b8c07d8231753268d76bbf945d
Kwenela Tubbs <kwenelatubbs@pm.me>
2025-04-21 22:39:53 -0700

    uninstalled image compression vite plugin is not compatibility on build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import viteImagemin from "vite-plugin-imagemin"; // New import
 
 export default defineConfig(({ mode }) => ({
   server: {
@@ -15,17 +14,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
-    viteImagemin({ // New plugin
-      gifsicle: { optimizationLevel: 3 },
-      mozjpeg: { quality: 70 },
-      pngquant: { quality: [0.7, 0.8] },
-      svgo: {
-        plugins: [
-          { name: 'removeViewBox' },
-          { name: 'removeEmptyAttrs', active: false }
-        ]
-      }
-    }),
   ].filter(Boolean),
   resolve: {
     alias: {
